47d84f29647e0e8a1bf71cd63081b1df9a1d48cf
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-14 11:42:37 -0700

    Updated

--- database/index.js
+++ database/index.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://mongo:27017/navigation', { useNewUrlParser: true })
+  .connect(
+    'mongodb://ec2-54-193-38-138.us-west-1.compute.amazonaws.com:27017/navigation',
+    {
+      useNewUrlParser: true
+    }
+  )
   .then(() => console.log('Connection to mongoDB is successful!'))
   .catch(err => console.log(err));
 
-//mongodb://localhost/navigation
+// mongodb://localhost/navigation
+
+//mongodb://mongo:27017/navigation
