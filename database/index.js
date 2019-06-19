7af2d0eb65c8f5113ebcb1ba23252217ab84e570
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-19 09:26:12 -0700

    updated loader

--- database/index.js
+++ database/index.js
@@ -10,4 +10,4 @@ mongoose
   .then(() => console.log('Connection to mongoDB is successful!'))
   .catch(err => console.log(err));
 
-// mongodb:ec2-18-144-46-115.us-west-1.compute.amazonaws.com:27017/navigation
+// mongodb://ec2-18-144-46-115.us-west-1.compute.amazonaws.com:27017/navigation
