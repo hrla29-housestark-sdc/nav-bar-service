dd406cf54a33590f2061cf846f2b079bd168c431
Wendy Huynh <wendyhuynhs@gmail.com>
2019-06-20 11:51:22 -0700

    updated mongo connection

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb://ec2-18-144-46-115.us-west-1.compute.amazonaws.com:27017/navigation',
+    'mongodb://ec2-54-193-16-236.us-west-1.compute.amazonaws.com:27017/navigation',
     {
       useNewUrlParser: true
     }
